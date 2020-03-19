b7c2fb399d854f561ce5d7982dd8e8447657d69a
maixuanvinh007 <maixuanvinh007@gmail.com>
2020-03-19 10:42:49 +0700

    task_D:Cart_creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    module CurrentCart
+        private
+        def set_cart
+        @cart = Cart.find(session[:cart_id])
+        rescue ActiveRecord::RecordNotFound
+        @cart = Cart.create
+        session[:cart_id] = @cart.id
+        end
+    end
 end
