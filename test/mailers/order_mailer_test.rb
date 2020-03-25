ce7312683bb753e4a552493fa2d7d6603f485652
maixuanvinh007 <maixuanvinh007@gmail.com>
2020-03-25 10:35:31 +0700

    fix_error_2

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class OrderMailerTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderMailer.received
-    assert_equal "Received", mail.subject
-    assert_equal ["maixuanvinh007@gmail.com"], mail.to
-    assert_equal ["maixuanvinh007@gmail.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "Pragmatic Store Order Confirmation", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /Programming Ruby 1.9/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderMailer.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["maixuanvinh007@gmail.com"], mail.to
-    assert_equal ["maixuanvinh007@gmail.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Pragmatic Store Order Shipped", mail.subject
+    assert_equal ["dave@example.org"], mail.to
+    assert_equal ["depot@example.com"], mail.from
+    assert_match /Programming Ruby 1.9/, mail.body.encoded
   end
 
 end
